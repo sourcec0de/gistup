cc99a864b348b2fea9cf8637da2e0d76339e651a
James R. Qualls <Qualls.James@gmail.com>
2014-12-09 11:34:50 -0500

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+gistup
+=======
+
+A command line utility for publishing and updating gists on your github account.
+
+
+```
+Usage:
+    gistup publish
+    gistup scripts
+    gistup open
+    gistup --help
+
+Details:
+    publish                  - publishes CWD to a gist (excluding dot files)
+    scripts                  - generates html script tags for gist files
+    open                     - opens the gist in default web browser
+
+Flags:
+    --help                   - displays this help text
+
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='gistup',
+      packages=['gistup'],
+      version='0.1',
+      description='A command line utility for publishing a directory and '
+                  'updating gists on your github account.',
+      author='James R. Qualls',
+      author_email='qualls.james@gmail.com',
+      url='https://github.com/sourcec0de/gistup',
+      download_url='https://github.com/sourcec0de/gistup/tarball/0.1',
+      keywords=['gist', 'blogging', 'command line', 'github', 'publish'],
+      classifiers=[])
