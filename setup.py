5ea0cf834a12c6ef2ae13f4760e3b129007c4105
James R. Qualls <Qualls.James@gmail.com>
2014-12-09 14:20:23 -0500

    pushing to pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from distutils.core import setup
 
-setup(name='gistup',
-      packages=['gistup'],
+desc = ('A command line utility for publishing a directory and '
+        'updating gists on your github account.')
+
+setup(name='gist-up',
+      packages=['gist-up'],
       version='0.1',
-      description='A command line utility for publishing a directory and '
-                  'updating gists on your github account.',
+      description=desc,
       author='James R. Qualls',
       author_email='qualls.james@gmail.com',
       url='https://github.com/sourcec0de/gistup',
       download_url='https://github.com/sourcec0de/gistup/tarball/0.1',
       keywords=['gist', 'blogging', 'command line', 'github', 'publish'],
+      scripts=['bin/gistup'],
       classifiers=[])
